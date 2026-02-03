a0509482ce62c2de6d3c4170f79996910d3008ac
abhi-7867 <abhisekasinha43@gmail.com>
2026-02-03 17:05:37 +0530

    feat(model): add core identity fields to Student

--- Student.java
+++ Student.java
@@ -6,4 +6,7 @@ import java.io.Serializable;
  */
 public class Student implements Serializable {
     private static final long serialVersionUID = 1L;
+    
+    private String name;
+    private String rollNumber;
 }
