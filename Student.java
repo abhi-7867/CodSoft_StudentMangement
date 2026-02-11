24c7421f8039aa28821269cda1be4e90605c8e6d
abhi-7867 <abhisekasinha43@gmail.com>
2026-02-11 18:17:51 +0530

    feat(model): add getters and setters for email and phone number

--- Student.java
+++ Student.java
@@ -60,4 +60,20 @@ public class Student implements Serializable {
     public void setAge(int age) {
         this.age = age;
     }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
